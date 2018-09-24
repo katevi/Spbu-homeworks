1a5dd5ec8b370d1cd1380868fbc9e490e7aa9f7f
Kate Vinnik <kate.vinnik393@gmail.com>
2018-09-24 19:44:44 +0300

    Fixed mistakes: added gap, indent, initialization, renamed currentCellofArray1

--- sem1/hw1/Vinnik_task3.cpp
+++ sem1/hw1/Vinnik_task3.cpp
@@ -1,45 +1,45 @@
 #include <iostream>
 
-void swap(int &currentCellOfArray1, int &currentCellOfArray2)
+void swap(int &a, int &b)
 {
-    int t = currentCellOfArray1;
-    currentCellOfArray1 = currentCellOfArray2;
-    currentCellOfArray2 = t;
+	int t = a;
+	a = b;
+	b = t;
 }
 
 void reverse(int currentArray[], int start, int end)
 {
-    {
-        int j = 0;
-        for (int i = start; i <= (end + start) / 2; i++)
-        {
-            j = j + 1;
-            swap(currentArray[i], currentArray[end - j + 1]);
-        }
-    }
-    
+	{
+		int j = 0;
+		for (int i = start; i <= (end + start) / 2; i++)
+		{
+			j = j + 1;
+			swap(currentArray[i], currentArray[end - j + 1]);
+		}
+	}
+
 }
 
-int main() 
+int main()
 {
-    std::cout << "Enter length of array (n)";
-    int n;
-    std::cin >> n;
-    std::cout << "Enter pointer (m)";
-    int m;
-    std::cin >> m;
-    int a[n];
-    std::cout << "Start array: ";
-    for (int i = 1; i <= n; i++)
-    {
-        a[i] = i;
-    }
-    
-    reverse(a, 1, m);
-    reverse(a, m + 1, n);
-    reverse(a,1,n);
+	std::cout << "Enter length of array (n)";
+	int n = 0;
+	std::cin >> n;
+	std::cout << "Enter pointer (m)";
+	int m = 0;
+	std::cin >> m;
+	int a[n];
+	std::cout << "Start array: ";
+	for (int i = 1; i <= n; i++)
+	{
+		a[i] = i;
+	}
 
-    std::cout << "Final array: ";
-    for (int i = 1; i <= n; i++)
-    std::cout << a[i];
+	reverse(a, 1, m);
+	reverse(a, m + 1, n);
+	reverse(a, 1, n);
+
+	std::cout << "Final array: ";
+	for (int i = 1; i <= n; i++)
+		std::cout << a[i];
 }
