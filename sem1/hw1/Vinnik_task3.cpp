4053b84dca7776435817b61236a75ac3145fc247
Kate Vinnik <kate.vinnik393@gmail.com>
2018-09-29 09:35:14 +0300

    Changed dinamic array on static

--- sem1/hw1/Vinnik_task3.cpp
+++ sem1/hw1/Vinnik_task3.cpp
@@ -22,24 +22,23 @@ void reverse(int currentArray[], int start, int end)
 
 int main()
 {
-	std::cout << "Enter length of array (n)";
-	int n = 0;
-	std::cin >> n;
+	const int _ARRAYLENGTH = 10;
 	std::cout << "Enter pointer (m)";
 	int m = 0;
 	std::cin >> m;
-	int a[n];
-	std::cout << "Start array: ";
-	for (int i = 1; i <= n; i++)
+	int a[_ARRAYLENGTH];
+	std::cout << "Start array (before reverse): ";
+	for (int i = 0; i < _ARRAYLENGTH; i++)
 	{
 		a[i] = i;
+		std::cout << a[i] << " ";
 	}
+	std::cout << "\n";
+	reverse(a, 0, m - 1);
+	reverse(a, m, _ARRAYLENGTH - 1);
+	reverse(a, 0, _ARRAYLENGTH - 1);
 
-	reverse(a, 1, m);
-	reverse(a, m + 1, n);
-	reverse(a, 1, n);
-
-	std::cout << "Final array: ";
-	for (int i = 1; i <= n; i++)
-		std::cout << a[i];
+	std::cout << "Final array (after reverse): ";
+	for (int i = 0; i < _ARRAYLENGTH; i++)
+		std::cout << a[i] << " ";
 }
