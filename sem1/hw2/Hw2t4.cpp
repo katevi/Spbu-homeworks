8377e7b750435e1d7deaa113cf9e763a6ff082d2
Kate Vinnik <kate.vinnik393@gmail.com>
2018-10-17 19:17:44 +0300

    Fixed indents

--- sem1/hw2/Hw2t4.cpp
+++ sem1/hw2/Hw2t4.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-
 int digitsSort(int digits[])
 {
-    for (int i = 0; i < 10; i++)
+	for (int i = 0; i < 10; i++)
 		for (int j = 0; j < 9; j++)
 			if (digits[j] > digits[j + 1])
 			{
@@ -11,25 +10,25 @@ int digitsSort(int digits[])
 				digits[j + 1] = digits[j];
 				digits[j] = t;
 			}
-    return digits[10];
+	return digits[10];
 }
 
 int main()
 {
-    const int numberOfCells = 10;
-    std::cout << "Enter the number:";
-    int number;
-    std::cin >> number;
-    
-    int digitsOfNumber[numberOfCells];
-    for (int i = 0; i < numberOfCells; i++)
-    digitsOfNumber[i] = -1;
-    
-    int i = number;
-    int j = 0;
+	const int numberOfCells = 10;
+	std::cout << "Enter the number:";
+	int number;
+	std::cin >> number;
+
+	int digitsOfNumber[numberOfCells];
+	for (int i = 0; i < numberOfCells; i++)
+		digitsOfNumber[i] = -1;
+
+	int i = number;
+	int j = 0;
 	int countZero = 0;
-    while (i > 0)
-    {
+	while (i > 0)
+	{
 		if (i % 10 == 0)
 		{
 			countZero++;
@@ -41,19 +40,17 @@ int main()
 			i = i / 10;
 			j++;
 		}
-    }
-    
-    digitsSort(digitsOfNumber);
-    
-    i = 0;
-    while (digitsOfNumber[i] == -1)
-        i++;
-
-    std::cout << "The smallest number formed by the digits of a given number:";
+	}
+	digitsSort(digitsOfNumber);
+	i = 0;
+	while (digitsOfNumber[i] == -1)
+	{
+		i++;
+	}
+	std::cout << "The smallest number formed by the digits of a given number:";
 	std::cout << digitsOfNumber[i];
 	for (int k = 1; k <= countZero; k++)
 		std::cout << 0;
-    for (int j = i + 1; j < numberOfCells; j++)
-        std::cout << digitsOfNumber[j];
-	system("pause");
+	for (int j = i + 1; j < numberOfCells; j++)
+		std::cout << digitsOfNumber[j];
 }
