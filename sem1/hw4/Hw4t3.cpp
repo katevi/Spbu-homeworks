2ccb6008eb8f025eab9dbd7bf9a4d2bf2292cab4
Kate Vinnik <kate.vinnik393@gmail.com>
2018-10-20 11:20:25 +0300

    Fixed fopen_s

--- sem1/hw4/Hw4t3.cpp
+++ sem1/hw4/Hw4t3.cpp
@@ -1,26 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>   
+#include <cstdio>
 
 int main()
 {
-	FILE *file = fopen("cppstudio.txt", "r");   
+	FILE* file;
+	file = fopen("cppstudio.txt", "r");
 	char currentSymbol = ' ';
 	int count = 0;
 	bool isNotEmpty = false;
-	while (!feof(file)) 
+	if (file == NULL) perror("Error opening file");
+	else
 	{
-		currentSymbol = fgetc(file);
-		if ((currentSymbol != ' ') and (currentSymbol != '\n') and (currentSymbol != '\t'))
+		while (!feof(file))
 		{
-			isNotEmpty = true;
+			currentSymbol = fgetc(file);
+			if ((currentSymbol != ' ') and (currentSymbol != '\n') and (currentSymbol != '\t'))
+				isNotEmpty = true;
+			if ((currentSymbol == '\n') and (isNotEmpty))
+			{
+				count++;
+				isNotEmpty = false;
+			}
+			if ((currentSymbol == EOF) and (isNotEmpty) and (count > 0))
+				count++;
 		}
-		if ((currentSymbol == '\n') and (isNotEmpty))
-		{
-			count++;
-			isNotEmpty = false;
-		}
-		if ((currentSymbol == EOF) and (isNotEmpty) and (count > 0))
-			count++;
 	}
 	std::cout << "Number of non-empty rows:" << count;
 	fclose(file);
+	system("pause");
 }
