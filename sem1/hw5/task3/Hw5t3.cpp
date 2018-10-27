ccb3172cb2ab4003bef6d585f5bf43dbd3a4089c
Kate Vinnik <kate.vinnik393@gmail.com>
2018-10-27 14:07:53 +0300

    Replaced array of char on string

--- sem1/hw5/task3/Hw5t3.cpp
+++ sem1/hw5/task3/Hw5t3.cpp
@@ -1,57 +1,61 @@
 #include <iostream>
 #include "Stack.h"
+#include <string>
 
-void writePriority1Operand(Stack* stack, char array[], int i)
+void writePriority1Operand(Stack* stack, std::string array, int i, std::string &finalString)
 {
 	if ((getLastElement(stack) == '+') || (getLastElement(stack) == '-') || (getLastElement(stack) == '(') || (isEmpty(stack)))
 		push(stack, array[i]);
 	else
 	{
-		std::cout << pop(stack) << " ";
+		finalString = finalString + pop(stack);
 		push(stack, array[i]);
 	}
 }
 
-void writePriority2Operand(Stack* stack, char array[], int i)
+void writePriority2Operand(Stack* stack, std::string array, int i, std::string &finalString)
 {
 	if (isEmpty(stack))
 		push(stack, array[i]);
 	else
 	{
 		while ((getLastElement(stack) != '(') && (!isEmpty(stack)))
-			std::cout << pop(stack) << " ";
+			finalString = finalString + pop(stack);
 		push(stack, array[i]);
 	}
 }
 
 int main()
 {
-	char expression[100] { ' ' };
+	std::string expression;
+	std::string finalString;
 	std::cout << "Enter expression:\n";
-	std::cin >> expression;
+	std::getline(std::cin, expression);
 	Stack* stack = createStack();
 	std::cout << "Reverse polish notation:\n";
-	for (int i = 0; i < strlen(expression); i++)
+	int lengthOfExpression = expression.length();
+	for (int i = 0; i < lengthOfExpression; i++)
 	{
 		if ((int(expression[i]) > 47) && (int(expression[i]) < 58))
-			std::cout << expression[i] << " ";
+			finalString = finalString + expression[i];
 		else
 		{
 			if ((expression[i] == '+') || (expression[i] == '-'))
-				writePriority2Operand(stack, expression, i);
+				writePriority2Operand(stack, expression, i, finalString);
 			if ((expression[i] == '*') || (expression[i] == '/'))
-				writePriority1Operand(stack, expression, i);
+				writePriority1Operand(stack, expression, i, finalString);
 			if (expression[i] == '(')
 				push(stack, expression[i]);
 			if (expression[i] == ')')
 			{
 				while (getLastElement(stack) != '(')
-					std::cout << pop(stack) << " ";
+					finalString = finalString + pop(stack);
 				pop(stack);
 			}
 		}
 	}
 	while (!isEmpty(stack))
-		std::cout << pop(stack) << " ";
+		finalString = finalString + pop(stack);
+	std::cout << finalString;
 	deleteStack(stack);
 }
