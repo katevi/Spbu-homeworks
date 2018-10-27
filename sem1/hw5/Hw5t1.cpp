688c31a493e80798f6cbd3a9d9612e538d5c60da
Kate Vinnik <kate.vinnik393@gmail.com>
2018-10-27 15:55:52 +0300

    Added brackets

--- sem1/hw5/Hw5t1.cpp
+++ sem1/hw5/Hw5t1.cpp
@@ -6,35 +6,49 @@ int main()
 	std::cout << "Enter side of array n*n (side is not even!)";
 	std::cin >> sideOfArray;
 	int **numbers = new int*[sideOfArray] {0};
-
 	for (int i = 0; i < sideOfArray; i++)
+	{
 		numbers[i] = new int[sideOfArray] {0};
-
+	}
 	int i = sideOfArray * sideOfArray;
 	int j = 0; 
 	int currentCoil = 0;
 	int numberOfCoils = sideOfArray / 2;
 	for (currentCoil = 1; currentCoil <= numberOfCoils; currentCoil++)
 	{
-		for (j = currentCoil - 1; j < sideOfArray - currentCoil + 1; j++) 
+		for (j = currentCoil - 1; j < sideOfArray - currentCoil + 1; j++)
+		{
 			numbers[currentCoil - 1][j] = i--;
-		for (j = currentCoil; j < sideOfArray - currentCoil + 1; j++) 
+		}
+		for (j = currentCoil; j < sideOfArray - currentCoil + 1; j++)
+		{
 			numbers[j][sideOfArray - currentCoil] = i--;
-		for (j = sideOfArray - currentCoil - 1; j >= currentCoil - 1; --j) 
+		}
+		for (j = sideOfArray - currentCoil - 1; j >= currentCoil - 1; --j)
+		{
 			numbers[sideOfArray - currentCoil][j] = i--;
-		for (j = sideOfArray - currentCoil - 1; j >= currentCoil; j--) 
+		}
+		for (j = sideOfArray - currentCoil - 1; j >= currentCoil; j--)
+		{
 			numbers[j][currentCoil - 1] = i--;
+		}
 	}
 	numbers[numberOfCoils][numberOfCoils] = 1;
 	std::cout << "Array, obtained by spiraling from the center: \n";
 	for (int i = 0; i < sideOfArray; i++)
+	{
 		for (int j = 0; j < sideOfArray; j++)
 		{
 			std::cout << numbers[i][j] << " ";
 			if (j == sideOfArray - 1)
+			{
 				std::cout << "\n";
+			}
 		}
+	}
 	for (int i = 0; i < sideOfArray; i++)
+	{
 		delete[] numbers[i];
+	}
 	delete[] numbers;
 }
