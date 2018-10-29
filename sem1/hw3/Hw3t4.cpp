4277888f107d534f010686f128c317e345fd7350
Kate Vinnik <kate.vinnik393@gmail.com>
2018-10-29 22:26:40 +0300

    Made random number generation with non-repeating digits

--- sem1/hw3/Hw3t4.cpp
+++ sem1/hw3/Hw3t4.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 
 void numberToArray(int array[], int number)
 {
@@ -9,6 +10,7 @@ void numberToArray(int array[], int number)
 		number = number / 10;
 	}
 }
+
 
 void findingBull(int arrayHidden[], int arrayCurrent[], int &bull)
 {
@@ -35,20 +37,41 @@ void findingCow(int arrayHidden[], int arrayCurrent[], int &cow)
 
 }
 
+void generatingOfHiddenNumber(int arrayOfDigits[], int &number)
+{
+	bool isDifferent = false;
+	for (int i = 1; i < 4; i++)
+	{
+		isDifferent = false;
+		while (!isDifferent)
+		{
+			isDifferent = true;
+			arrayOfDigits[i] = rand() % 9 + 0;
+			for (int j = 0; j < i; j++)
+			{
+				if (arrayOfDigits[j] == arrayOfDigits[i])
+					isDifferent = false;
+			}
+		}
+	}
+	number = arrayOfDigits[3] + arrayOfDigits[2] * 10 + arrayOfDigits[1] * 100 + arrayOfDigits[0] * 1000;
+}
+
 int main()
 {
-	int const constHiddenNumber = 7593;
+	srand(time(NULL));
+	int digitsHiddenNumber[4] {-1};
+	digitsHiddenNumber[0] = rand() % 9 + 1;
+	int constHiddenNumber = 0;
+	generatingOfHiddenNumber(digitsHiddenNumber, constHiddenNumber);
 	int hiddenNumber = constHiddenNumber;
 	int currentNumber = 0;
-	int digitsHiddenNumber[4]{ 0 };
-	int digitsCurrentNumber[4]{ 0 };
-	numberToArray(digitsHiddenNumber, hiddenNumber);
+	int digitsCurrentNumber[4] {0};
 	int bull = 0;
 	int cow = 0;
 	int count = 1;
-
-	std::cout << "A (bull) - means your number has a number that is the original number and is in the same place as the original number \n";
-	std::cout << "B (cow) - means your number has a number that is the original number, but is not in the same place as the original number \n";
+	std::cout << "A (bull) - your number has the same number and same place \n";
+	std::cout << "B (cow) - your number has the same number, but not on the same place \n";
 	std::cout << "You have ten attempts to guess the hidden number. Enter numbers...\n";
 	while (!((bull == 4) && (cow == 0)))
 	{
