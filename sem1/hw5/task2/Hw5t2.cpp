07fbea65f8c2419b63e39fae75b6e79b5ae42df7
Kate Vinnik <kate.vinnik393@gmail.com>
2018-10-31 22:10:00 +0300

    Redid the task (without magic constants)

--- sem1/hw5/task2/Hw5t2.cpp
+++ sem1/hw5/task2/Hw5t2.cpp
@@ -4,42 +4,36 @@ int main()
 {
 	FILE* file = fopen("Words.txt", "r");
 	char currentSymbol = ' ';
-	char previousSymbol = ' ';
-	//52 - number of big and small letters in english language
-	char letters[52];
-	for (int i = 0; i < 26; i++)
-	{
-		letters[i] = char(65 + i);
-	}
-
-	for (int i = 26; i < 52; i++)
-	{
-		letters[i] = char(97 + (i % 26));
-	}
-
+	char lettersOfWord[50];
+	int size = 0;
 	printf("The first occurrence of the letters in the words: \n");
-	int numberOfLetters[52] {0};
 	while (!feof(file))
 	{
 		currentSymbol = fgetc(file);
-		for (int i = 0; i < 52; i++)
+		if (currentSymbol == ' ') 
 		{
-			if ((currentSymbol == letters[i]) && (numberOfLetters[i] == 0))
+			for (int i = 0; i < size; i++)
 			{
-				printf("%c", currentSymbol);
-				numberOfLetters[i]++;
-				break;
+				lettersOfWord[i] = ' ';
 			}
+			size = 0;
+			printf("%c", ' ');
 		}
-		if (currentSymbol == ' ')
+		else
 		{
-			for (int i = 0; i < 52; i++)
+			size++;
+			bool isFirst = true;
+			for (int i = 0; i < size; i++)
 			{
-				numberOfLetters[i] = 0;
+				if (currentSymbol == lettersOfWord[i])
+					isFirst = false;
 			}
-			printf(" ");
+			if (isFirst)
+			{
+				lettersOfWord[size - 1] = currentSymbol;
+				printf("%c", currentSymbol);
+			}
 		}
 	}
-
 	fclose(file);
 }
