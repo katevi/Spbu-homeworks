f75ba8a8560b37b8a6dc1232905e428c41bb9991
Kate Vinnik <kate.vinnik393@gmail.com>
2018-12-21 15:04:13 +0300

    Fixed mistakes

--- sem1/test/task1/Test3t1.cpp
+++ sem1/test/task1/Test3t1.cpp
@@ -7,13 +7,18 @@ using namespace std;
 
 int const stringSize = 256;
 
-void printStringsToFile(HashTable* table, ofstream &file)
+
+void sort(char** strings, HashTable* table)
 {
-	for (int i = 0; i < table->size; i++)
-	{
-		if (table->bucket[i] != nullptr)
-		{
-			file << table->bucket[i]->word->string << "\n";
+	char* temp = 0;
+	for (int i = 0; i < table->currentSize - 1; i++) {
+		for (int j = 0; j < table->currentSize - i - 1; j++) {
+			if (strcmp(strings[j], strings[j + 1]) == 1)
+			{
+				temp = strings[j];
+				strings[j] = strings[j + 1];
+				strings[j + 1] = temp;
+			}
 		}
 	}
 }
@@ -30,7 +35,23 @@ int main()
 		addElement(table, string);
 	}
 	ofstream output("output.txt");
-	printStringsToFile(table, output);
+	char** strings = new char*[table->currentSize];
+	int k = 0;
+	for (int i = 0; i < table->size; i++)
+	{
+		if (table->bucket[i] != nullptr)
+		{
+			strings[k] = table->bucket[i]->word->string;
+			k++;
+		}
+	}
+	sort(strings, table);
+	for (int i = 0; i < table->currentSize; i++)
+	{
+		output << strings[i] << endl;
+	}
+
+	delete[] strings;
 	deleteTable(table);
 	output.close();
 	file.close();
