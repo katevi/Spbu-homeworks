57e677987e1a0ead20c4a1643f8cdcb210be3af2
Kate Vinnik <kate.vinnik393@gmail.com>
2019-03-02 13:21:45 +0300

    Added Hw2t2

---
+++ sem2/hw2/task2/src/main/java/Vinnik/g144/ArrayStack.java
@@ -0,0 +1,54 @@
+package Vinnik.g144;
+
+import java.util.EmptyStackException;
+
+/**Implements simple stack on an array*/
+public class ArrayStack <Type> implements Stack <Type> {
+    private int maximumSize = 3;
+    private int top = -1;
+    private Type[] stack = (Type[]) new Object[maximumSize];
+
+    /** @return - returns number of elements in stack*/
+    public int currentSize(){
+        return (this.top + 1);
+    }
+
+    private void doubleSize() {
+        Type[] temp = (Type[]) new Object[stack.length * 2];
+        System.arraycopy(stack,0, temp, 0, stack.length);
+        maximumSize = maximumSize * 2;
+        stack = temp;
+    }
+
+    /**@param value value of adding element */
+    @Override
+    public void push(Type value) {
+        if (maximumSize == currentSize()){
+            doubleSize();
+        }
+        this.top++;
+        stack[this.top] = value;
+    }
+
+    /** @return - returns value of the last element and removes it after */
+    @Override
+    public Type pop() throws EmptyStackException {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        this.top--;
+        return stack[this.top + 1];
+    }
+
+    /** @return - only returns value of the last element*/
+    @Override
+    public Type top() {
+        return stack[this.top];
+    }
+
+    /** @return - returns true if the stack is empty and false otherwise. */
+    @Override
+    public boolean isEmpty() {
+        return (this.top == -1);
+    }
+}
---
+++ sem2/hw2/task2/src/test/java/Vinnik/g144/LinkedStackTest.java
@@ -0,0 +1,42 @@
+package Vinnik.g144;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LinkedStackTest {
+
+    @Test
+    public void pop() {
+        LinkedStack stack = new LinkedStack();
+        stack.push(4);
+        stack.push(3);
+        assertEquals(3, stack.pop());
+        assertEquals(4, stack.pop());
+    }
+
+    @Test
+    public void isEmpty() {
+        LinkedStack stack = new LinkedStack();
+        boolean result = stack.isEmpty();
+        assertEquals(true, result);
+    }
+
+    /*@Test
+    public void size() {
+        LinkedStack stack = new LinkedStack();
+        stack.push(4);
+        stack.push(3);
+        assertEquals(2, stack.size());
+    }*/
+
+    @Test
+    public void top() {
+        LinkedStack stack = new LinkedStack();
+        stack.push(2);
+        stack.push(4);
+        stack.push(3);
+        assertEquals(3, stack.top());
+    }
+}
+
