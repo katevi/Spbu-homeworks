aeadedb72759b8ee04d4fe439c25a7c163bc5eef
Kate Vinnik <kate.vinnik393@gmail.com>
2019-03-02 11:14:29 +0300

    moved the StackElement class to the end, made it's fields private, fixed comments

--- sem2/hw1/task1/Stack.java
+++ sem2/hw1/task1/Stack.java
@@ -1,32 +1,16 @@
 package Vinnik.g144;
 
-/**
- * Implements simple stack.
- */
+/** Implements simple stack. */
 public class Stack<Type>{
-    private class StackElement {
-
-        public Type value;
-        public StackElement next;
-
-        public StackElement(Type value, StackElement next) {
-            this.value = value;
-            this.next = next;
-        }
-    }
 
     private StackElement head = null;
 
-    /**
-     * @param value - value of adding element
-     */
+    /** @param value - value of adding element */
     public void push(Type value){
         head = new StackElement(value, head);
     }
 
-    /**
-     * @return - returns value of the top element, removed element before.
-     */
+    /** @return - returns value of the top element, removed element before. */
     public Type pop() {
         if (!isEmpty()) {
             Type value = head.value;
@@ -36,17 +20,25 @@ public class Stack<Type>{
         return null;
     }
 
-    /**
-     * @return - returns true if the stack is empty and false otherwise.
-     */
+    /** @return - returns true if the stack is empty and false otherwise. */
     public boolean isEmpty() {
         return (head == null);
     }
 
-    /**
-     * @return - return the value of the top element
-     */
+    /** @return - return the value of the top element */
     public Type top(){
         return head.value;
     }
+
+    private class StackElement {
+
+        private Type value;
+        private StackElement next;
+
+        private StackElement(Type value, StackElement next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
 }
