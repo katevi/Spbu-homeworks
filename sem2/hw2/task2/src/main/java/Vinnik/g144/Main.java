fa994aefbe47879c44e8bb0e873437f6fa6f1ded
Kate Vinnik <kate.vinnik393@gmail.com>
2019-03-19 23:17:55 +0300

    Added Hw2t2

---
+++ sem2/hw2/task2/src/main/java/Vinnik/g144/Main.java
@@ -0,0 +1,15 @@
+package Vinnik.g144;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/** Implements a simple calculator of arithmetic expressions with digits. (Example (5+3-2*6))**/
+public class Main {
+
+    public static void main(String args[]) {
+        LinkedStack<Character> stack1 = new LinkedStack();
+        LinkedStack<Float> stack2 = new LinkedStack();
+        Calculator calculator = new Calculator(stack1, stack2);
+        String expression = calculator.input();
+        System.out.println("Value of this expression = " + calculator.calculate(expression));
+    }
+}
