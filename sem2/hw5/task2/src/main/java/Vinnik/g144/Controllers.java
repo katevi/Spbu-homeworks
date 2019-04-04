bd84a9fd61e46ca0bc4f12678b065ce0f8d32375
Kate Vinnik <kate.vinnik393@gmail.com>
2019-04-04 21:11:58 +0300

    Added JavaHw5t2

---
+++ sem2/hw5/task2/src/main/java/Vinnik/g144/Controllers.java
@@ -0,0 +1,75 @@
+package Vinnik.g144;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TextField;
+
+public class Controllers {
+
+    private ObservableList<String> operationsList = FXCollections.observableArrayList("+", "-", "/", "*");
+
+    @FXML
+    private Spinner <Integer> firstNumber, secondNumber;
+
+    @FXML
+    private ChoiceBox <String> operation;
+
+    @FXML
+    private TextField outputResult;
+
+    /** Initializing method.
+     * Sets the range of numbers, initial values of the numbers and the result to zero,
+     * adds list of the available operations with numbers.
+     * Calculates the result of applying an operation on two numbers.
+     */
+    @FXML
+    public void initialize() {
+        SpinnerValueFactory<Integer> valueFirst = new SpinnerValueFactory.IntegerSpinnerValueFactory(-1000, 1000, 0);
+        SpinnerValueFactory<Integer> valueSecond = new SpinnerValueFactory.IntegerSpinnerValueFactory(-1000, 1000, 0);
+
+        this.firstNumber.setValueFactory(valueFirst);
+        this.secondNumber.setValueFactory(valueSecond);
+        firstNumber.setEditable(true);
+        secondNumber.setEditable(true);
+        outputResult.setEditable(false);
+
+        this.operation.setItems(operationsList);
+        this.operation.setValue("");
+        outputResult.textProperty().setValue("0");
+
+        //Spinners to select numbers light up green during clicking on them
+        operation.valueProperty().addListener((observable, oldValue, newValue) -> calculate());
+        firstNumber.valueProperty().addListener((observable, oldValue, newValue) -> calculate());
+        secondNumber.valueProperty().addListener((observable, oldValue, newValue) -> calculate());
+    }
+
+    private void calculate() {
+        double result = 0;
+        int first = firstNumber.getValue();
+        int second = secondNumber.getValue();
+        //in the case of division by zero, the result will "infinity".
+        switch (this.operation.getValue()) {
+            case "+": {
+                result = first + second;
+                break;
+            }
+            case "-" : {
+                result = first - second;
+                break;
+            }
+            case "*" : {
+                result = first * second;
+                break;
+            }
+            case "/" : {
+                result = (double) first / second;
+                break;
+            }
+        }
+        outputResult.textProperty().setValue(String.valueOf(result));
+    }
+}
---
+++ sem2/hw5/task2/src/main/java/Vinnik/g144/Main.java
@@ -0,0 +1,29 @@
+package Vinnik.g144;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /** Starts the calculator application. */
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("Calculator.fxml"));
+            primaryStage.setTitle("Calculator:)");
+            primaryStage.setScene(new Scene(root, 600, 400));
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
