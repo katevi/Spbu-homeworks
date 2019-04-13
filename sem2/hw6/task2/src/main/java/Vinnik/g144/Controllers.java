b96738d85099f958ffe4512a3581d59ccdb71e59
Kate Vinnik <kate.vinnik393@gmail.com>
2019-04-14 00:19:21 +0300

    Fixed documentation

--- sem2/hw6/task2/src/main/java/Vinnik/g144/Controllers.java
+++ sem2/hw6/task2/src/main/java/Vinnik/g144/Controllers.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
-/** Handles communication with user (button presses). */
+/** Handles communication with user (button pressed). */
 public class Controllers {
 
     private Button[] buttons;
