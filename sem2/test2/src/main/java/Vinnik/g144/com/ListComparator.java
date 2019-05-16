dea1112b9da999f9e888aad7fcaa510f9035c2fd
Timofey Bryksin <timofey.bryksin@gmail.com>
2019-05-16 18:42:22 +0300

    Revert "Added fully working version (forgot to rename)"

--- sem2/test2/src/main/java/Vinnik/g144/com/ListComparator.java
+++
@@ -1,8 +0,0 @@
-package Vinnik.g144.com;
-
-import java.util.LinkedList;
-
-/** Compares two lists. */
-public  interface ListComparator<Type> {
-    int compare(LinkedList first, LinkedList second);
-}
