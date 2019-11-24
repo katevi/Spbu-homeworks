b536c9c215eb0a260b494493eb77ecd46a83e185
Kate <kate.vinnik393@gmail.com>
2019-11-24 23:14:40 +0300

    Added form for connection

--- sem3/hw4/task1/src/main/java/g144/Vinnik/Controller.java
+++ sem3/hw4/task1/src/main/java/g144/Vinnik/Controller.java
@@ -1,27 +1,28 @@
 package g144.Vinnik;
 
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.*;
 
-public class Controller extends JFrame {
-    private static void initialize() {
+public class Controller extends JDialog {
+    private JPanel contentPane;
+    private JButton clientFieldButton;
+    private JButton serverFieldButton;
+    private JTextField textField1;
+    private JButton connectButton;
+    private JButton buttonOK;
+    private JButton buttonCancel;
 
-    }
-
-    private void serverButtonAction() {
-
-
-    }
-
-    private void gameForServer() {
-
-    }
-
-    private void clientButtonAction() {
+    public Controller() {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
 
     }
 
-    private void gameForClient() {
-
+    public static void main(String[] args) {
+        Controller dialog = new Controller();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
     }
 }
