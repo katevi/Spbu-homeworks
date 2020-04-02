ad0154fc8ad650e098caf3a48d687599dbe15acd
Kate <kate.vinnik393@gmail.com>
2020-04-02 19:13:00 +0300

    Added monadic function

--- sem4/hw5/task3/LocalMax.hs
+++ sem4/hw5/task3/LocalMax.hs
@@ -1,5 +1,9 @@
 module LocalMax where
 
+import Control.Monad.Zip
+
 findLocalMax :: Ord a => [a] -> Maybe a
 findLocalMax xs = (\l -> if (null l) then Nothing else Just (head l)) 
-        (zip3 (drop 1 xs) xs (drop 2 xs) >>= (\(x, y, z) -> if (x > y && x > z) then [x] else []))
+        (mzip3 (drop 1 xs) xs (drop 2 xs) >>= (\(x, y, z) -> if (x > y && x > z) then [x] else []))
+
+mzip3 xs ys zs = mzipWith (\x (y, z) -> (x, y, z)) xs (mzip ys zs)
