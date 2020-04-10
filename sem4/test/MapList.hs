45efd01f4c18d5ca7f509184b0511f8b8a645de4
Kate <kate.vinnik393@gmail.com>
2020-04-10 17:33:36 +0300

    Swapped arguments to order as in example

--- sem4/test/MapList.hs
+++ sem4/test/MapList.hs
@@ -1,12 +1,12 @@
 module MapList where
 
-supermap :: (a -> [b]) -> [a] -> [b]
-supermap x ys = supermap' ys [[]] where
+supermap :: [a] -> (a -> [b]) -> [b]
+supermap ys x = supermap' ys [[]] where
         supermap' [] zs = reverse $ concat zs
         supermap' (w : ws) zs = supermap' ws $ (reverse $ x w) : zs
         
 
-test = (supermap (\t -> [t^2, t + 1])  [1,2,3]) == [1,2,4,3,9,4] &&
-       (supermap (\t -> [t + 2, t + 3]) [2,3,4]) == [4,5,5,6,6,7] &&
-       (supermap (\t -> [t + 1, t + 2, t + 3, t + 4]) [1]) == [2,3,4,5] &&
-       (supermap (\t -> [sin (pi * t)]) [1/2, 0]) == [1.0, 0.0]
+test = (supermap [1,2,3] (\t -> [t^2, t + 1])) == [1,2,4,3,9,4] &&
+       (supermap [2,3,4] (\t -> [t + 2, t + 3])) == [4,5,5,6,6,7] &&
+       (supermap [1] (\t -> [t + 1, t + 2, t + 3, t + 4])) == [2,3,4,5] &&
+       (supermap [1/2, 0] (\t -> [sin (pi * t)])) == [1.0, 0.0]
